b02019cedce337a8981a3ed99540bd8868de6b48
hairyf <wwu710632@gmail.com>
2025-05-09 13:14:35 +0800

    feat(config): update deployment configurations and add new network settings
    - Added deployment configurations for Moonchain Testnet and Mainnet.
    - Updated Solidity version and settings.
    - Introduced new scripts for deployment and verification in package.json.
    - Adjusted addresses.json to reflect correct network IDs.

--- harsta.config.ts
+++ harsta.config.ts
@@ -1,31 +1,67 @@
+import type { Deploy } from 'harsta'
+import process from 'node:process'
+import { Wallet } from 'ethers'
 import { defineConfig } from 'harsta'
+
+const deploy: Deploy = {
+  accounts: [
+    process.env.DEPLOYER_PRIVATE_KEY || Wallet.createRandom().privateKey,
+    process.env.VERIFIER_PRIVATE_KEY || Wallet.createRandom().privateKey,
+  ],
+  saveDeployments: true,
+  allowUnlimitedContractSize: true,
+  gas: 'auto',
+  gasPrice: 'auto',
+}
 
 const config = defineConfig({
   paths: {
     sources: './src/contracts',
     config: './src/config',
   },
+  solidity: {
+    settings: { evmVersion: 'shanghai' },
+    version: '0.8.24',
+  },
+  namedAccounts: {
+    deployer: { default: 0 },
+    verifier: { default: 1 },
+  },
   networks: {
-    goerli: {
-      name: 'Goerli',
-      rpc: 'https://rpc.ankr.com/eth_goerli',
+    moonchain_geneva: {
+      name: 'Moonchain Testnet',
+      rpc: 'https://geneva-rpc.moonchain.com',
       testnet: true,
-      id: 5,
-      currency: { name: 'Goerli Ether', symbol: 'ETH', decimals: 18 },
+      id: 5167004,
+      icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
+      currency: { decimals: 18, name: 'MXC Token', symbol: 'MXC' },
       explorer: {
-        name: 'Etherscan',
-        url: 'https://goerli.etherscan.io',
+        name: 'etherscan',
+        url: 'https://geneva-explorer.moonchain.com',
       },
+      deploy,
     },
-    ethereum: {
-      name: 'Ethereum',
-      rpc: 'https://eth.merkle.io',
-      id: 1,
-      currency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+    moonchain: {
+      name: 'Moonchain',
+      rpc: 'https://rpc.mxc.com',
+      id: 18686,
+      icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
+      currency: { decimals: 18, name: 'MXC Token', symbol: 'MXC' },
       explorer: {
-        name: 'Etherscan',
-        url: 'https://etherscan.io',
+        name: 'etherscan',
+        url: 'https://explorer.moonchain.com',
       },
+      deploy,
+    },
+  },
+  deployments: {
+    ERC20WithOwnable: {
+      kind: 'uups',
+      args: async env => [
+        await env.getNamedAccount('deployer') || await env.getUnnamedAccount(),
+        'ERC20WithOwnable',
+        'ERC20WithOwnable',
+      ],
     },
   },
 })
